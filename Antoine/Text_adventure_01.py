a98eeb636bf88347677ec2381a8b5c3f8f4f15b9
Blobatu <antoinedesjardinsc@icloud.com>
2026-01-27 13:11:07 -0500

    updated scene1

--- Antoine/Text_adventure_01.py
+++ Antoine/Text_adventure_01.py
@@ -16,17 +16,22 @@ def scene1():
     print_("You wake up from a deep slumber at a corner table in your local taverne.")
     print_("As you steadily regain consciousness, you notice a hooded figure sitting across from you.")
     print_("Just as you noticed it, it noticed you and is now rapidly approaching your table in strong, heavy strides.")
-    
+    print_("suddently, with a rush of adrenaline, you recall what it is you went to do here today,")
+    print_("it wasn't simply to get drunk, although that is an always pleasant side effewct of meetings in pubs.")
+    print_("Today was thge day where you would finally learn who sent that letter and why it was so vague.") #if an inventory system ever gets added, here would be th point to put a "mysterious letter added to inventory" line
+    print_("""Indeed, last week, you were invited to be here, at this moment, for an "opportunity". """)
+    print_("The hooded stranger sits down facing you and, without warning, unsheaths a dagger, propelling it's edge towards your throat")
+
 
     print("\n--- What do you do? ---")
-    print("option1")
-    print("option2")
-    print("option3")
-    path1 = input("choice: ")
+    print("(1) lean back with your entire body do try and avoid the blade")
+    print("(2) throw the table upwards to deflect the blow")
+    print("(3) nothing")
+    path1 = input("What do you do? (1/2/3): ")
     return path1
 
 #gameplay tree
-print(scene1())
+
 path1 = scene1()
 if path1 == "1":
     print_("choice1")
