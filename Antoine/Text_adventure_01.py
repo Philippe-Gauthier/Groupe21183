50b310dc036d632061c0a88dafd5d951605a6864
Blobatu <antoinedesjardinsc@icloud.com>
2026-01-30 15:48:20 -0500

    cleaned stuff up

--- Antoine/Text_adventure_01.py
+++ Antoine/Text_adventure_01.py
@@ -23,10 +23,10 @@ def scene1():
 
     print_("\n\nYou wake up from a deep slumber at a corner table in your local taverne.")
     print_("As you steadily regain consciousness, you notice a hooded figure sitting across from you.")
-    print_("Just as you noticed it, it noticed you and is now rapidly approaching your table in strong, heavy strides.")
+    print_("Just as you notice it, it notices you and is now rapidly approaching your table in strong, heavy strides.")
     print_("Suddently, with a rush of adrenaline, you recall what it is you went to do here today,")
-    print_("It wasn't simply to get drunk, although that is an always pleasant side effewct of meetings in pubs.")
-    print_("Today was thge day where you would finally learn who sent that letter and why it was so vague.") #if an inventory system ever gets added, here would be th point to put a "mysterious letter added to inventory" line
+    print_("It wasn't simply to get drunk, although that is an always pleasant side effect of meetings in pubs.")
+    print_("Today was the day where you would finally learn who sent that letter and why it was so vague.") #if an inventory system ever gets added, here would be th point to put a "mysterious letter added to inventory" line
     print_("""Indeed, last week, you were invited to be here, at this moment, for an "opportunity". """)
     print_("The hooded stranger sits down facing you and, without warning, unsheaths a dagger, propelling it's edge towards your throat")
 
@@ -42,7 +42,7 @@ def scene1():
 def scene1_1():
     print_("\n\nYou lean back with your entire body do try and avoid the blade")
     print_("It misses you by mere inches, then, just as swiftly as it had slashed, it retracted into it's sheath.")
-    print_("It all happened so fast that, for all else present, it looked only as if you had fallen to the ground from alcohol, nothing more.")
+    print_("It all happens so fast that, for all else present, it looks only as if you had fallen to the ground from alcohol, nothing more.")
     print_("The figure stands up and offers to help you back to your feet.")
     print_(""""You are indeed as swift and insightfull as described, my time is not being wasted here". he says.""")
     print_(""""Let's talk buisness", he continues,"as you have no doubt heard, multiple adventurers have recently lost their lives climbing the mountain" """)
@@ -71,7 +71,7 @@ def scene1_1_2():
 ##scene 1_2, throw table
 def scene1_2():
     print_("\n\nYou rapidly raise your knees, lunching the table upwards with all your strength.")
-    print_("The blade narrowly misses your chin, your legs it hurts and you're not one to leave pain unpunished.")
+    print_("The blade narrowly misses your chin, your legs hit the wood with immense strength, it hurts and you're not one to leave pain unpunished.")
     print_("Taking advantage of the ensuing cahos, you spring over the tavern table, now split in two by the impact with your assailant's arms.")
     print_("Pushing him to the rough floor, punching him bloody, you pull out your own dagger and place it on his throat.")
     print_("Shouting over his struggles, you demand to know who he is and why he attacked you.")
