c7bde10c394f252008331d1ea1ede66ca08b6411
Luc Desforges <desforgesluc00@gmail.com>
2026-02-24 15:07:51 -0500

    Projet 1 - Remise Finale - Luc Desforges
    Enjoy

--- LucDesforges/myproject/flaskr/auth.py
+++ LucDesforges/myproject/flaskr/auth.py
@@ -13,9 +13,9 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+bp = Blueprint('auth', __name__, url_prefix = '/auth')
 
-@bp.route('/register', methods=('GET', 'POST'))
+@bp.route('/register', methods = ('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -44,7 +44,7 @@ def register():
 
     return render_template('auth/register.html')
 
-@bp.route('/login', methods=('GET', 'POST'))
+@bp.route('/login', methods = ('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
